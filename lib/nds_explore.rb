ae645d661311dec268b2766140225e3ba825c14a
Donny Landis <donny.landis@gmail.com>
2020-02-12 17:22:47 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,5 +8,9 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-
+  row_index = 0
+  while row_index < directors_database.count do
+    puts "Row #{row_index} has #{directors_database[row_index]} columns"
+  end
+  row_index += 1
 end
